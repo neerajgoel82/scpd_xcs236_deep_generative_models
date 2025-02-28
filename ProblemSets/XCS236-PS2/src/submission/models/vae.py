Fixing the issue with kl divergence computation

ProblemSets/XCS236-PS2/src/submission/models/vae.py
@@ -120,6 +120,9 @@ class VAE(nn.Module):
         batch = x.shape[0]
         z_prior_means = self.z_prior[0].expand(batch, self.z_dim)
         z_prior_variances = self.z_prior[1].expand(batch, self.z_dim)
+        duplicated_z_prior_means = ut.duplicate(z_prior_means, iw)
+        duplicated_z_prior_variances = ut.duplicate(z_prior_variances, iw)
+
 
         #compute reconstruction loss
         q_phi = self.enc(x)
@@ -135,11 +138,10 @@ class VAE(nn.Module):
         rec = ut.log_mean_exp(log_p_theta_image_wise, 0) * -1
 
         #compute kl divergence 
-        kl_image_wise = ut.kl_normal(q_phi[0], q_phi[1], z_prior_means, z_prior_variances)
-        kl = torch.mean(kl_image_wise)
-
-        #print some variables 
-        #self.print_1b_variables(q_phi, z_pred, x_pred_logits, log_p_theta_image_wise)
+        kl_log_normal = ut.log_normal(z_pred, duplicated_means, duplicated_variance)
+        kl_log_normal_with_prior = ut.log_normal(z_pred,duplicated_z_prior_means,duplicated_z_prior_variances)
+        kl_image_wise = kl_log_normal - kl_log_normal_with_prior
+        kl = ut.log_mean_exp(kl_image_wise, 0)
 
         #compute nelbo
         nelbo = kl + rec